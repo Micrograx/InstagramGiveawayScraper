3a64d3abd470323470c8c6fff64430dc3c78addc
Micrograx <Valentinocerutti@gmail.com>
2020-11-26 23:32:27 -0300

    Adds constants file

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Defining easy to break constants for future fixing
+
+# --------------- COMMENT GATHERING ---------------
+
+# Button for loading more comments XPath:
+BTN_MORE_COMMENTS = '//*[@id="react-root"]/section/main/div/ul/li/div/button'
+
+# Each comment class name:
+CLASS_COMMENT = "C4VMK"
+
+# Each comment username class name:
+CLASS_COMMENT_USERNAME = "_6lAjh"
+
+
+# --------------- LIKES GATHERING ---------------
+
+# Link to likes XPath:
+BTN_ALL_LIKES = '//*[@id="react-root"]/section/main/div/div/article/div[3]/section[2]/div/div/a'
+
+# Each Like XPath to count them:
+XPATH_SINLGE_COMMENT = '//*[@id="react-root"]/section/main/div[1]/div'
--- giveaway.py
+++ giveaway.py
@@ -4,7 +4,9 @@ import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from secrets import LOGIN, PASSWORD
+from constants import *
 import re
+
 
 class Giveaway:
     def __init__(self, username, password, give_away_people):
@@ -13,8 +15,8 @@ class Giveaway:
         self.password = password
         self.path = f'{os.getcwd()}/chromedriver'
         mobile_emulation = {
-        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+            "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+            "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
         chrome_options = Options()
         chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
         self.driver = webdriver.Chrome(self.path, chrome_options=chrome_options)
@@ -34,30 +36,28 @@ class Giveaway:
     def close_browser(self):
         self.driver.close()
 
-    
     def get_comments(self):
         self.driver.get('https://www.instagram.com/p/B4NssH8iVmJ/comments/')
         time.sleep(1)
         while True:
             try:
-                button = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/ul/li/div/button')
+                button = self.driver.find_element_by_xpath(BTN_MORE_COMMENTS)
                 button.click()
                 time.sleep(1)
             except Exception:
                 print("Got all comments!")
                 break
-        comments = self.driver.find_elements_by_class_name("C4VMK")
+        comments = self.driver.find_elements_by_class_name(CLASS_COMMENT)
         comments_list, users_list = [], []
         for comment in comments:
             ig_comment = comment.find_element_by_css_selector('span').get_attribute('textContent')
-            ig_user = comment.find_element_by_class_name('TlrDj').get_attribute('textContent')
+            ig_user = comment.find_element_by_class_name(CLASS_COMMENT_USERNAME).get_attribute('textContent')
             comments_list.append(ig_comment)
             users_list.append(ig_user)
         comments_list = comments_list[1:]
         users_list = users_list[1:]
         profiles = self.check_comments(users_list, comments_list)
         return profiles
-
 
     def good_comment(self, comment):
         users = re.findall(r'@[\w\.-]+', comment)
@@ -75,25 +75,24 @@ class Giveaway:
                 profiles.append(user)
         return profiles
 
-    
     def get_people_who_liked(self):
         self.driver.get("https://www.instagram.com/p/B4NssH8iVmJ")
-        link_to_likes = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/section[2]/div/div/a')
+        link_to_likes = self.driver.find_element_by_xpath(BTN_ALL_LIKESd)
         link_to_likes.click()
         time.sleep(2)
-        current = len(self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div[1]/div').find_elements_by_tag_name('a'))
+        current = len(self.driver.find_element_by_xpath(XPATH_SINLGE_COMMENT).find_elements_by_tag_name('a'))
         while True:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(random.randint(1,2))
-            new = len(self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div[1]/div').find_elements_by_tag_name('a'))
+            time.sleep(random.randint(1, 2))
+            new = len(self.driver.find_element_by_xpath(XPATH_SINLGE_COMMENT).find_elements_by_tag_name('a'))
             if new == current:
                 print("Got all likes...")
                 break
             current = new
-        all_links = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div[1]/div').find_elements_by_tag_name('a')
+        all_links = self.driver.find_element_by_xpath(XPATH_SINLGE_COMMENT).find_elements_by_tag_name('a')
         people = self.extract_likes(all_links)
         return people
-    
+
     def extract_likes(self, all_links):
         people = set()
         for link in all_links:
