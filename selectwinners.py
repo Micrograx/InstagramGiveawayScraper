74bc4436730e22cf8c9f97e6b278e083cbe6080f
Micrograx <Valentinocerutti@gmail.com>
2020-11-29 12:58:40 -0300

    Adds argumment handling

--- selectwinners.py
+++ selectwinners.py
@@ -3,8 +3,35 @@ from secrets import LOGIN, PASSWORD
 import time, sys, getopt
 
 def main(argv):
-    giveaway_people = ['@jakobowsky', '@someone']
-    ig = Giveaway(LOGIN, PASSWORD, giveaway_people)
+
+    allow_duplicates = False
+    tag_ammount = 0
+    require_likes = False
+    require_comments = False
+    excluded_accounts = []
+
+    try:
+        opts, args = getopt.getopt(argv, "hdt:lce:", ["tags=", "excluded=", "help"])
+    except getopt.GetoptError:
+      print 'selectwinners.py -t <Ammount of tag per comment> -e <Usernames excluded>'
+      sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print 'selectwinners.py -t <Ammount of tag per comment> -e <Usernames excluded>'
+            sys.exit()
+        elif opt == "-d":
+            allow_duplicates = True
+        elif opt in ("-t", "--tags"):
+            tag_ammount = arg
+        elif opt  == "-l":
+            require_likes = True
+        elif opt == "-c":
+            require_comments = True
+        elif opt in ("-e", "--excluded"):
+            excluded_accounts = arg
+
+    ig = Giveaway(LOGIN, PASSWORD, excluded_accounts)
     ig.login()
     print("Logged in...")
     print("Getting people who commented and tagged 2 friends...")
